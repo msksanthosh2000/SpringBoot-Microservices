358071aad9961faad172b4d79063bf345340be8e
SANTHOSH KUMAR M <msksanthosh2000@gmail.com>
2024-10-05 16:08:41 +0530

    Added Service Registry

---
+++ service-registry/src/test/java/com/Microservice/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservice.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
